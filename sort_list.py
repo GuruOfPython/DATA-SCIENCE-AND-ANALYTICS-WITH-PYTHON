99162289a3694274ab879518273bd10bf121dd06
GuruOfPython <vpeKJmPc@mail.com>
2018-12-18 18:38:33 -0800

    update of sort_list

--- sort_list.py
+++ sort_list.py
@@ -6,12 +6,32 @@ import natsort
 
 ## Make a list of file names like P_0.png, P_1.png
 file_names = [f'P_{index}.png' for index in range(200)]
-print(file_names)
+print('New List\t', file_names)
 
 ## Sort by sort()
 file_names.sort()
-print(file_names)
+print('Sort by sort\t', file_names)
+
+## Sort by index
+def takeFirstElement(elem):
+    return elem[0]
+
+file_names.sort(key=takeFirstElement)
+print('Sort by index\t', file_names)
+
+## Sort by sorted
+student_tuples = [
+        ('john', 'A', 15),
+        ('jane', 'B', 12),
+        ('dave', 'B', 10),
+]
+sorted(student_tuples, key=lambda student: student[2])   # sort by age
+print('Sort by sorted\t', student_tuples)
 
 ## Sort by natsort
 file_names = natsort.natsorted(file_names, reverse=False)
-print(file_names)
+print('Sort by natsort\t', file_names)
+
+
+
+
