2cb2cccdacdb4fbe9547c5fc54340538f8fdb059
GuruOfPython <vpeKJmPc@mail.com>
2018-12-24 09:07:24 -0800

    Update of convert_string_to_time

--- convert_string_to_time.py
+++ convert_string_to_time.py
@@ -11,5 +11,10 @@ first_day = datetime(cur_year, cur_month, 1)
 print(first_day)
 
 dt = datetime.strptime('2018-12-25', '%Y-%m-%d')
+
 print(dt < first_day)
 
+str = "Dec 3, 2018"
+import re
+match = re.findall(r"([A-Za-z]+)\s?([0-9]+),\s?([0-9]+)", str)[0]
+print(match)
