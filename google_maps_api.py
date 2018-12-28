7e2aae418dd26490b0ee4599157e55992fdbccc2
GuruOfPython <vpeKJmPc@mail.com>
2018-12-28 05:23:25 -0800

    Update of google maps api

--- google_maps_api.py
+++ google_maps_api.py
@@ -1,7 +1,7 @@
 import requests
 
 addr = "2945 Irvington Rd, Falls Church, VA, 22042-1607"
-api_key = "AIzaSyAo6qNQJMhz2br7FbUbVcb7krKz-26VyZ0"
+api_key = "XXXX"
 url = f"http://maps.googleapis.com/maps/api/streetview?fov=70&location={addr}&size=600x400&key={api_key}"
 
 print(url)
@@ -12,6 +12,16 @@ headers = {
 r = requests.get(url=url, headers = headers, stream=True)
 
 save_file_name = '1.png'
+with open(save_file_name, "wb") as png:
+    for chunk in r.iter_content(chunk_size=1024):
+        if chunk:
+            png.write(chunk)
+
+url = f"https://maps.googleapis.com/maps/api/staticmap?center={addr}&zoom=16&size=600x400&maptype=roadmap&markers=color:blue%7Clabel:%7C{addr}&key={api_key}"
+print(url)
+r = requests.get(url=url, headers = headers, stream=True)
+
+save_file_name = '2.png'
 with open(save_file_name, "wb") as png:
     for chunk in r.iter_content(chunk_size=1024):
         if chunk:
