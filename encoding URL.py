cb0438c56cb39ac8cb4b4d71910d0337c5b6c0b0
GuruOfPython <vpeKJmPc@mail.com>
2019-01-25 14:35:17 -0800

    2nd Commit

--- encoding URL.py
+++ encoding URL.py
@@ -1,4 +1,5 @@
 from urllib.parse import urlencode
+
 url = "http://in-stjoseph-assessor.governmax.com/propertymax/search_property.asp?l_nm=owner&user=guest_in-stjoseph-assessor&pass=manatron&sid={}"
 
 query = {
@@ -9,5 +10,19 @@ query = {
 }
 
 query_encoded = urlencode(query)
+new_url = "http://in-stjoseph-assessor.governmax.com/propertymax/search_property.asp?" + query_encoded
+print(new_url)
+
+sid = 'F83D36C5F0094B86B4BE9307649DA66F'
+full_name = "Michael+Jason"
+query = {
+    'sid': sid,
+    'l_nm': 'owner',
+    'l_wc': '|name_search=CONTAINS|n.own_name='+full_name,
+    'name_search': 'CONTAINS',
+    'n.own_name': full_name
+}
+
+query_encoded = urlencode(query).replace("%7C", "|").replace('%3D', '=').replace('%2B', "+")
 new_url = "http://in-stjoseph-assessor.governmax.com/propertymax/search_property.asp?" + query_encoded
 print(new_url)
